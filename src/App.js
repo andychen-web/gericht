1210c252d63157e7a5767e8816d69df8f55f1d4c
andychen <descartes1960s@gmail.com>
2023-05-18 10:34:47 +0800

    feat:create Header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/custom.css";
 import Navigation from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import images from "../data/images";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundImage: `url(${images.bg})`,
+      }}
+    >
+      <Container className=" py-5">
+        <Row>
+          <Col
+            xs={12}
+            md={6}
+            className="d-flex flex-column justify-content-center"
+          >
+            <h2 className="text-capitalize text-white">追尋新風味</h2>
+            <img height={"auto"} width={"40px"} src={images.spoon} alt="" />
+            <span className="text-capitalize fw-bold custom-text fs-1">
+              探索精緻飲食
+            </span>
+            <p className="text-muted">
+              品味創意菜餚，沉浸於令人心馳神往的味覺饗宴，彷彿探險家在未知的味道中遨遊
+            </p>
+            <button className="custom-btn">菜單</button>
+          </Col>
+          <Col xs={12} md={6}>
+            <img width={"100%"} src={images.welcome} alt="welcome" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;

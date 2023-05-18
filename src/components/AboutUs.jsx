6a446cd0a88b558948297166622910450d405596
andychen <descartes1960s@gmail.com>
2023-05-18 16:37:09 +0800

    refactor: improve AboutUs UI in smaller screen

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,20 +8,19 @@ const AboutUs = () => {
       <Container className="py-5">
         <Row>
           <Col
-            xs={10}
+            xs={12}
             md={5}
             className="d-flex flex-column align-items-end justify-content-center"
           >
             <span className="text-capitalize fw-bold custom-text fs-1">
               關於我們
             </span>
-            <h5 className="text-white py-1 text-end w-75">
-              我們充滿熱情，致力於提供卓越的產品和服務。我們深信唯有不斷探索和創新才能滿足
-              客戶的需求。
+            <h5 className="text-white py-1 xs:text-start md:text-end w-75">
+              我們充滿熱情，致力於提供卓越的產品和服務。我們深信唯有不斷探索和創新才能滿足客戶的需求。
             </h5>
             <button className="custom-btn">了解更多</button>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={3} md={2}>
             <img
               className="position-relative top-50 "
               width={"100%"}
@@ -37,14 +36,14 @@ const AboutUs = () => {
             />
           </Col>
           <Col
-            xs={12}
+            xs={9}
             md={5}
-            className="d-flex flex-column align-items-start justify-content-center"
+            className="py-3 d-flex flex-column align-items-start justify-content-center"
           >
             <span className="text-capitalize fw-bold custom-text fs-1">
               特色料理
             </span>
-            <h5 className="text-white py-1 text-start w-75">
+            <h5 className="text-white py-1 text-start xs:w-100 md:w-75">
               我們承諾為每一位客戶提供無與倫比的價值和卓越的服務。我們了解客戶的需求、提供客製菜餚
             </h5>
             <button className="custom-btn">了解更多</button>

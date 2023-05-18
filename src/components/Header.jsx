e25c9ea1f3d1a1d9b4f8749c09eaf7f217180ce9
andychen <descartes1960s@gmail.com>
2023-05-18 13:37:25 +0800

    chore: remove unnecessary styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,7 @@ import images from "../data/images";
 
 const Header = () => {
   return (
-    <div
-      className="bg"
-    >
+    <div className="bg">
       <Container className="py-5">
         <Row>
           <Col
@@ -14,11 +12,9 @@ const Header = () => {
             md={6}
             className="d-flex flex-column justify-content-center"
           >
-            <h2 className="text-capitalize text-white">追尋新風味</h2>
+            <h2 className="text-white">追尋新風味</h2>
             <img height={"auto"} width={"40px"} src={images.spoon} alt="" />
-            <span className="text-capitalize fw-bold custom-text fs-1">
-              探索精緻飲食
-            </span>
+            <span className="fw-bold custom-text fs-1">探索精緻飲食</span>
             <h5 className="text-white py-1">
               品味創意菜餚，沉浸於令人心馳神往的味覺饗宴，彷彿探險家在未知的味道中遨遊
             </h5>

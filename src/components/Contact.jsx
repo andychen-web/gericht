db76dd2f3af4f77364b6f84c6410e3c1468c3631
andychen <descartes1960s@gmail.com>
2023-05-22 15:44:27 +0800

    feat: create Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "react-bootstrap/container";
+import Row from "react-bootstrap/row";
+import Col from "react-bootstrap/col";
+import images from "../data/images";
+import Title from "./Title";
+
+const Contact = () => {
+  return (
+    <div className="bg py-5" id="contact-us">
+      <Container>
+        <Row>
+          <Col
+            xs={12}
+            md={6}
+            className="d-flex flex-column justify-content-center py-4"
+          >
+            <Title title={"聯絡資訊"} subTitle={"聯絡我們"} />
+            <h5 className="text-white py-3">新竹市東區美食路33號</h5>
+            <span className="golden-text fs-3"> 營業時間</span>
+            <span className="text-white">Mon - Fri: 10:00 am - 02:00 am</span>
+            <span className="text-white">Sat - Sun: 10:00 am - 03:00 am</span>
+          </Col>
+          <Col xs={12} md={6}>
+            <img width={"90%"} src={images.findus} alt="findUs" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Contact;

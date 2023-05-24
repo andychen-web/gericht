b76ddfd17e81a028b837c8ee7c5a2bc904a511a1
andychen <descartes1960s@gmail.com>
2023-05-24 15:13:32 +0800

    feat: add img animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,14 +16,18 @@ const Header = () => {
             className="d-flex flex-column justify-content-center"
           >
             <Title title={"追尋新風味"} subTitle={"探索精緻飲食"} />
-            <h5 className="text-white py-4 w-75">
-              品嘗菜餚，享受令人嚮往的味覺饗宴
-              <br /> 彷彿探險家在未知的味道中遨遊
+            <h5 className="text-white py-4 pe-3 w-75">
+              品嘗菜餚，享受令人嚮往的味覺饗宴， 彷彿探險家在未知的味道中遨遊
             </h5>
             <button className="custom-btn">菜單</button>
           </Col>
           <Col xs={12} md={6}>
-            <img width={"100%"} src={images.welcome} alt="welcome" />
+            <img
+              width={"100%"}
+              className="img-animation"
+              src={images.welcome}
+              alt="welcome"
+            />
           </Col>
         </Row>
       </Container>

6853977b2bc522a139e5c41b160ad43198c1adc8
andychen <descartes1960s@gmail.com>
2023-08-17 17:24:25 +0800

    style: navbar位置與間隔

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import images from "../data/images";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 const Navigation = () => {
   return (
@@ -9,18 +10,16 @@ const Navigation = () => {
       bg="black"
       expand="lg"
       variant="dark"
-      className="position-fixed overflow-hidden"
-      style={{ zIndex: 3 }}
+      className="position-fixed overflow-hidden px-5"
+      style={{ zIndex: 3, width: "100vw" }}
     >
-      <div className="container-fluid" style={{ width: "100vw" }}>
-        <Navbar.Brand className="w-50" href="/">
+      <Container fluid>
+        <Navbar.Brand href="/">
           <img src={images.gericht} alt="logo" className="w-50" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse
-          id="navbarNav"
-        >
-          <Nav className="ps-5 ms-5">
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="ms-auto pe-5">
             <Nav.Link className="custom-link" href="/">
               首頁
             </Nav.Link>
@@ -32,7 +31,7 @@ const Navigation = () => {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
-      </div>
+      </Container>
     </Navbar>
   );
 };

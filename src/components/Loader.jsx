91b3b85ab4f4e0ff3a11e7855623f987520a4163
andychen <descartes1960s@gmail.com>
2023-08-30 18:00:35 +0800

    feat:優化Products頁面使用體驗

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 import { ColorRing } from "react-loader-spinner";
 import PropTypes from "prop-types";
+import Modal from "react-bootstrap/Modal";
 
 const Loader = ({ isLoading }) => {
   Loader.propTypes = {
     isLoading: PropTypes.bool,
   };
   return (
-    <ColorRing
-      visible={isLoading}
-      height="100%"
-      width="100%"
-      ariaLabel="blocks-loading"
-      wrapperStyle={{
-        zIndex: 1,
-        paddingRight: "400px",
-        paddingLeft: "400px",
-        paddingBottom: "30%",
-        paddingTop: "10%",
-        background: "rgba(128, 128, 128, 0.5)",
-      }}
-      wrapperClass="blocks-wrapper position-absolute top-0"
-      colors={["#e15b64", "#f47e60", "#f8b26a", "#f8b26a", "#e15b64"]}
-    />
+    <Modal show={isLoading} className="d-flex justify-content-center">
+      <ColorRing
+        visible={isLoading}
+        height="100"
+        width="100"
+        ariaLabel="blocks-loading"
+        wrapperClass="blocks-wrapper position-absolute mt-5"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#f8b26a", "#e15b64"]}
+      />
+    </Modal>
   );
 };
 export default Loader;
